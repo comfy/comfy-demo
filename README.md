066b2f741f23982f19d19e5ec6a1fd3f67b4b5cb
Oleg <oleg@khabarov.ca>
2013-10-29 19:42:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Comfy Demo
+
+Just a simple rails application with `comfortable_mexican_sofa` installed
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ComfyDemo::Application.config.secret_key_base = 'd67b4b684fa28c22fc16da8c3f4f6bfc77f0812486d27e1e9a145a70fb7e5b5c154a44ef952944b085057979bac28ff5cf982e06234c8035b65a90ff6a88f58c'
