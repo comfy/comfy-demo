2243b70226717e8b3a377c29a06d1858f6a70297
Oleg <oleg@khabarov.ca>
2017-11-12 18:08:22 -0800

    clean slate with rails 5.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  comfy_route :cms_admin, :path => '/admin'
-
-  # Make sure this routeset is defined last
-  comfy_route :cms, :path => '/', :sitemap => false
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
