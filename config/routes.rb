b797f7e828d44041bd20f77b3bbc9fb4eb63dc84
Oleg <oleg@khabarov.ca>
2017-11-18 15:04:09 -0800

    happy enough with custom rendering methods

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
     resources :shoes
   end
 
+  get "/with-cms-fragments",      to: "custom#with_cms_fragments"
+  get "/with-cms-page",           to: "custom#with_cms_page"
+  get "/with-cms-page-explicit",  to: "custom#with_cms_page_explicit"
+  get "/with-cms-layout",         to: "custom#with_cms_layout"
+
   comfy_route :cms_admin, path: "/admin"
   # Ensure that this route is defined last
   comfy_route :cms, path: "/"
